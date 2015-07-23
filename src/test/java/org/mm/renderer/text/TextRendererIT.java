2682a095d728caafbbb42bad21faf5bc59a73a97
martinjoconnor <martin.oconnor@stanford.edu>
2015-07-23 16:56:18 -0700

    First text renderer integration test

--- src/test/java/org/mm/renderer/text/TextRendererIT.java
+++ src/test/java/org/mm/renderer/text/TextRendererIT.java
@@ -1,18 +1,36 @@
 package org.mm.renderer.text;
 
-import org.junit.Assert;
-import org.junit.Before;
+import junit.framework.Assert;
+import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WriteException;
 import org.junit.Test;
+import org.mm.exceptions.MappingMasterException;
+import org.mm.parser.ParseException;
+import org.mm.rendering.text.TextRendering;
+import org.mm.ss.SpreadsheetLocation;
+import org.mm.test.IntegrationTestBase;
 
-public class TextRendererIT 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+
+public class TextRendererIT extends IntegrationTestBase
 {
-  @Before
-  public void setUp() 
-  {
-  }
+  private static final String SHEET_NAME1 = "Sheet1";
+  private SpreadsheetLocation currentLocation = new SpreadsheetLocation(SHEET_NAME1, 1, 1);
 
-  @Test
-  public void TestOWLClassDeclaration()
+  @Test public void TestOWLClassDeclaration()
+    throws WriteException, BiffException, MappingMasterException, ParseException, IOException
   {
+    String expression = "Class: @A1";
+    String expectedRendering = "Class: Car";
+    Label cellA1 = createCell("Car", 1, 1);
+    Set<Label> cells = createCells(cellA1);
+    Optional<? extends TextRendering> textRendering = createTextRendering(SHEET_NAME1, cells, currentLocation,
+      expression);
+
+    Assert.assertTrue(textRendering.isPresent());
+    Assert.assertEquals(textRendering.get().getTextRendering(), expectedRendering);
   }
 }
