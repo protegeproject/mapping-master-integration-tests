99e529798922893d6171f6ddcde7c0ab5c959f5a
zippyrate <martin@zippyrate.com>
2015-07-23 20:03:28 -0700

    Some more integration tests

--- src/test/java/org/mm/renderer/text/TextRendererIT.java
+++ src/test/java/org/mm/renderer/text/TextRendererIT.java
@@ -20,7 +20,7 @@ public class TextRendererIT extends IntegrationTestBase
   private static final String SHEET_NAME1 = "Sheet1";
   private SpreadsheetLocation currentLocation = new SpreadsheetLocation(SHEET_NAME1, 1, 1);
 
-  @Test public void TestOWLClassDeclaration()
+  @Test public void TestAbsoluteReference()
     throws WriteException, BiffException, MappingMasterException, ParseException, IOException
   {
     String expression = "Class: @A1";
@@ -33,4 +33,34 @@ public class TextRendererIT extends IntegrationTestBase
     Assert.assertTrue(textRendering.isPresent());
     Assert.assertEquals(textRendering.get().getTextRendering(), expectedRendering);
   }
+
+	@Test public void TestColumnWildcardReference()
+			throws WriteException, BiffException, MappingMasterException, ParseException, IOException
+	{
+		String expression = "Class: @*1";
+		String expectedRendering = "Class: Car";
+		Label cellA1 = createCell("Car", 1, 1);
+		Set<Label> cells = createCells(cellA1);
+		SpreadsheetLocation currentLocation = new SpreadsheetLocation(SHEET_NAME1, 1, 1);
+		Optional<? extends TextRendering> textRendering = createTextRendering(SHEET_NAME1, cells, currentLocation,
+				expression);
+
+		Assert.assertTrue(textRendering.isPresent());
+		Assert.assertEquals(textRendering.get().getTextRendering(), expectedRendering);
+	}
+
+	@Test public void TestRowWildcardReference()
+			throws WriteException, BiffException, MappingMasterException, ParseException, IOException
+	{
+		String expression = "Class: @A*";
+		String expectedRendering = "Class: Car";
+		Label cellA1 = createCell("Car", 1, 1);
+		Set<Label> cells = createCells(cellA1);
+		SpreadsheetLocation currentLocation = new SpreadsheetLocation(SHEET_NAME1, 1, 1);
+		Optional<? extends TextRendering> textRendering = createTextRendering(SHEET_NAME1, cells, currentLocation,
+				expression);
+
+		Assert.assertTrue(textRendering.isPresent());
+		Assert.assertEquals(textRendering.get().getTextRendering(), expectedRendering);
+	}
 }
